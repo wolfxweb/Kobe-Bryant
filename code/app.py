e9b1224e3361cfd3c91191936cb4b0b8ee57eafb
Eduardo lobo <wolfxweb@gmail.com>
2024-04-11 19:29:46 -0300

    executado para recriar os graficos

--- code/app.py
+++ code/app.py
@@ -7,12 +7,9 @@ from sklearn import metrics
 prod_file = '../data/processed/prediction_prod.parquet'
 dev_file = '../data/processed/prediction_test.parquet'
 
-
 df_prod = pd.read_parquet(prod_file)
 df_dev = pd.read_parquet(dev_file)
 
-# st.write(df_prod)
-# st.write(df_dev)
 st.title("Painel de Controle")
 st.subheader("Engenharia de Machine Learning")
 st.markdown("""
@@ -23,13 +20,12 @@ fignum = plt.figure(figsize=(6,4))
 # Saida do modelo dados dev
 sns.distplot(df_dev.prediction_score_1,
              label='Teste',
-             ax = plt.gca())
+             ax=plt.gca())
 
 # Saida do modelo dados prod
 sns.distplot(df_prod.predict_score,
              label='Producao',
-             ax = plt.gca())
-
+             ax=plt.gca())
 
 plt.title('Monitoramento Desvio de Dados da Saída do Modelo')
 plt.ylabel('Densidade Estimada')
@@ -40,9 +36,8 @@ plt.legend(loc='best')
 
 st.pyplot(fignum)
 
-
 try:
-    st.subheader("Classsification report base desenvolvimento")
+    st.subheader("Classification report base desenvolvimento")
     target_column = 'shot_made_flag'
     if target_column in df_dev.columns:
         report = metrics.classification_report(df_dev.prediction_label, df_dev[target_column], output_dict=True)
@@ -50,5 +45,15 @@ try:
         st.write(report_df)
     else:
         st.write(f"Coluna  '{target_column}' não encontrada")
+
+    st.subheader("Matriz de Confusão")
+    cm = metrics.confusion_matrix(df_dev.prediction_label, df_dev[target_column])
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', cbar=False, ax=ax)
+    ax.set_xlabel('Rótulos Previstos')
+    ax.set_ylabel('Rótulos Verdadeiros')
+    ax.set_title('Matriz de Confusão')
+    st.pyplot(fig)
+
 except KeyError as e:
     st.write(f"Error: {e}")
